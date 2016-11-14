b798caa5a362cd70a3d096cb58d68b0c23d559d2
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-11-14 11:25:17 -0600

    added db connection

---
+++ src/customer-service/Data/BangazonConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace customer_service.Data
+{
+    public class BangazonConnection
+    {
+        private string _path = System.Environment.GetEnvironmentVariable("BangazonWeb_Db_Path");
+
+        public void insert(string query)
+        {
+            SqliteConnection dbcon = new SqliteConnection(_path);
+
+            dbcon.Open();
+            SqliteCommand dbcmd = dbcon.CreateCommand();
+
+            dbcmd.CommandText = query;
+            dbcmd.ExecuteNonQuery();
+
+            // clean up
+            dbcmd.Dispose();
+            dbcon.Close();
+        }
+
+        public void execute(string query, Action<SqliteDataReader> handler)
+        {
+
+            SqliteConnection dbcon = new SqliteConnection(_path);
+
+            dbcon.Open();
+            SqliteCommand dbcmd = dbcon.CreateCommand();
+            dbcmd.CommandText = query;
+
+            using (var reader = dbcmd.ExecuteReader())
+            {
+                handler(reader);
+            }
+
+            // clean up
+            dbcmd.Dispose();
+            dbcon.Close();
+        }
+    }
+}
