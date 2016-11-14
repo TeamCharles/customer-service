4ec8011c3f7fd109ee53226fa862745e38789545
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-11-14 14:59:37 -0600

    failing test

---
+++ customer-service-tests/CustomerFactoryTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using customer_service.Models;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace customer_service_tests
+{
+    public class CustomerFactoryTests
+    {
+        [Fact]
+        public void CustomerModelCreatesNewCustomer()
+        {
+            Customer customer = new Customer();
+            customer.CustomerId = 9999;
+            customer.DateCreated = DateTime.Now;
+            customer.FirstName = "Garrett";
+            customer.LastName = "Vangilder";
+            Assert.Equal(9999, customer.CustomerId);
+            Assert.NotNull(customer.DateCreated);
+            Assert.Equal("Garrett", customer.FirstName);
+            Assert.Equal("Vangilder", customer.LastName);
+        }
+
+        [Fact]
+        public void 
+    }
+}
