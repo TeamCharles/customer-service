117a06d68c8649609abd4aadd8e569d93986f204
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-11-14 11:32:50 -0600

    updated documentation

--- src/customer-service/Data/BangazonConnection.cs
+++ src/customer-service/Data/BangazonConnection.cs
@@ -3,10 +3,26 @@ using Microsoft.Data.Sqlite;
 
 namespace customer_service.Data
 {
+    /**
+     * Class: BangazonConnection
+     * Purpose: Connects App with DB allows for executing and reading commands
+     * Author: Matt Hamil/Garrett Vangilder
+     * Methods:
+     *     
+     *     void insert(string query) - This method allows for users to insert new information into a SQLite database
+     *     void execute(string query, Action<SqliteDataReader> handler) - This method opens the database, executes a given command(handler) and then closes the connection
+     */
     public class BangazonConnection
     {
         private string _path = System.Environment.GetEnvironmentVariable("BangazonWeb_Db_Path");
 
+        /**
+         * Purpose: Inserts new information into the datase
+         * Arguments:
+         *     query - database query
+         * Return:
+         *     n/a 
+         */
         public void insert(string query)
         {
             SqliteConnection dbcon = new SqliteConnection(_path);
@@ -21,7 +37,14 @@ namespace customer_service.Data
             dbcmd.Dispose();
             dbcon.Close();
         }
-
+        /**
+         * Purpose: Execute SQL statement
+         * Arguments:
+         *     query - database query
+         *     Action<SqliteDataReader> handler - callback function to be executed during connection
+         * Return:
+         *     n/a 
+         */
         public void execute(string query, Action<SqliteDataReader> handler)
         {
 
@@ -36,7 +59,6 @@ namespace customer_service.Data
                 handler(reader);
             }
 
-            // clean up
             dbcmd.Dispose();
             dbcon.Close();
         }
