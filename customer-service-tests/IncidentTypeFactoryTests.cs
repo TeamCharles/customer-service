f4a822124786b61dab1bede8ab4330ba866d8fbc
Matt Kraatz <Matt.Kraatz@gmail.com>
2016-11-14 13:51:22 -0800

    completed IncidentTypeFactory tests and dev

---
+++ customer-service-tests/IncidentTypeFactoryTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using customer_service;
+using customer_service.Models;
+
+namespace customer_service_tests
+{
+    public class IncidentTypeFactoryTests
+    {
+        [Fact]
+        public void CanInstantiateIncidentTypeFactory()
+        {
+            var fact = IncidentTypeFactory.Instance;
+            Assert.NotNull(fact);
+        }
+
+        [Fact]
+        public void IncidentTypeFactoryIsSingleton()
+        {
+            var fact1 = IncidentTypeFactory.Instance;
+            var fact2 = IncidentTypeFactory.Instance;
+            Assert.Equal(fact1, fact2);
+        }
+
+        [Fact]
+        public void CanGetAllIncidentTypes()
+        {
+            var types = IncidentTypeFactory.Instance.getAll();
+            Assert.IsType<List<IncidentType>>(types);
+            Assert.NotNull(types[0].IncidentTypeId);
+            Assert.NotNull(types[0].Label);
+        }
+    }
+}
---
+++ src/customer-service/Factories/IncidentTypeFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using customer_service.Data;
+using customer_service.Models;
+using Microsoft.Data.Sqlite;
+
+namespace customer_service
+{
+    public class IncidentTypeFactory
+    {
+        // Make the factory a singleton to maintain state across all uses
+        private static IncidentTypeFactory _instance;
+        public static IncidentTypeFactory Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new IncidentTypeFactory();
+                }
+                return _instance;
+            }
+        }
+
+        // Get all customers
+        public List<IncidentType> getAll()
+        {
+            BangazonConnection conn = new BangazonConnection();
+            List<IncidentType> list = new List<IncidentType>();
+
+            // Execute the query to retrieve all customers
+            conn.execute(@"select 
+				IncidentTypeId,
+				Label 
+				from IncidentType",
+                (SqliteDataReader reader) =>
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new IncidentType
+                        {
+                            IncidentTypeId = reader.GetInt32(0),
+                            Label = reader[1].ToString()
+                        });
+                    }
+                }
+            );
+
+            return list;
+        }
+    }
+}
