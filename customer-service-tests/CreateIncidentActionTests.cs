a45325fb7fc65ebb02e03e865e829667c16087c1
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-11-15 13:48:41 -0600

    WIP

---
+++ customer-service-tests/CreateIncidentActionTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace customer_service_tests
+{
+    public class CreateIncidentActionTests
+    {
+    }
+}
---
+++ src/customer-service/Actions/CreateIncidentAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace customer_service.Actions
+{
+    public class CreateIncidentAction
+    {
+        [Fact]
+        public void CanSaveNewIncidentToDatabase()
+        {
+            var incidentFactory = new IncidentFactory();
+            List<Incident> allIncidents = incidentFactory.getAll();
+            int newIncidentId = allIncidents[allIncidents.Count].IncidentId;
+            Assert.NotNull(instance);
+
+            Incident BadIncident = new Incident();
+        }
+    }
+}
