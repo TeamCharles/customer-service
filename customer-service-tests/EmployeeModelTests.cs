cf8e29388b6e9e3d81b22fb6bcfddf23cae95aba
Matt Hamil <matthamil@users.noreply.github.com>
2016-11-16 10:30:01 -0600

    14-save-employee :: Can save new employees to DB (#29)
    * WIP
    
    * WIP
    
    * WIP
    
    * EmployeeModelTests complete
    
    * Fixed test bug
    
    * Kraatz suggestions

---
+++ customer-service-tests/EmployeeModelTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using customer_service;
+using customer_service.Models;
+using customer_service.Data;
+
+namespace customer_service_tests
+{
+    public class EmployeeModelTests
+    {
+        [Fact]
+        public void CanSaveNewEmployeeToDatabase()
+        {
+            var employeeFactory = new EmployeeFactory();
+            List<Employee> allEmployees = employeeFactory.getAll();
+            int newEmployeeId = allEmployees[allEmployees.Count - 1].EmployeeId + 1;
+
+            Employee Jeb = new Employee()
+            {
+                FirstName = "Jeb",
+                LastName = "Bush",
+                DepartmentId = 1,
+                Administrator = false
+            };
+
+            Jeb.save();
+
+            var shouldBeJeb = employeeFactory.get(newEmployeeId);
+
+            Assert.NotNull(shouldBeJeb);
+            Assert.NotNull(shouldBeJeb.EmployeeId);
+            Assert.True(newEmployeeId == shouldBeJeb.EmployeeId);
+            Assert.True(Jeb.FirstName == shouldBeJeb.FirstName);
+            Assert.True(Jeb.LastName == shouldBeJeb.LastName);
+            Assert.True(Jeb.DepartmentId == shouldBeJeb.DepartmentId);
+            Assert.True(Jeb.Administrator == shouldBeJeb.Administrator);
+
+            var conn = new BangazonConnection();
+            conn.insert($"DELETE FROM Employee WHERE EmployeeId = {shouldBeJeb.EmployeeId}");
+        }
+    }
+}
