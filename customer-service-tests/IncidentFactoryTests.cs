ae175ef70ad0eb8dff2ad365c70faf1bbb60827d
Dayne Wright <daynewr@gmail.com>
2016-11-16 22:38:57 -0600

    line 50 corrected to IncidentFactory.ParseDate(date)

--- customer-service-tests/IncidentFactoryTests.cs
+++ customer-service-tests/IncidentFactoryTests.cs
@@ -47,7 +47,7 @@ namespace customer_service_tests
         public void CanParseDate(string date)
         {
             var fact = new IncidentFactory();
-            DateTime? parsedDate = fact.ParseDate(date);
+            DateTime? parsedDate = IncidentFactory.ParseDate(date);
             Assert.Equal<DateTime?>(new DateTime(2016, 05, 10), parsedDate);
         }
     }
